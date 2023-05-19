fe850e7812e7780a9639e663ea8e735db2c8fef5
Patrick Bacza <Patrick.Bacza@outlook.com>
2023-05-19 17:34:45 -0400

    got rif of custom exporter

--- Extraction/stock_data/stock_data/custom_exporter.py
+++
@@ -1,6 +0,0 @@
-from scrapy.exporters import CsvItemExporter
-
-class CustomCsvItemExporter(CsvItemExporter):
-    def __init__(self, *args, **kwargs):
-        kwargs['delimiter'] = '|'  # Set the desired delimiter
-        super().__init__(*args, **kwargs)
