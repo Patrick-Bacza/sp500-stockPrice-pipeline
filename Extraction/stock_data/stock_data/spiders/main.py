e0ec8e407dd89e47ff312730a905351fe6c4198a
Patrick Bacza <Patrick.Bacza@outlook.com>
2023-06-20 21:55:43 -0400

    fixed errors caused by adding index to extraction

--- Extraction/stock_data/stock_data/spiders/main.py
+++ Extraction/stock_data/stock_data/spiders/main.py
@@ -8,10 +8,11 @@ from datetime import date
 
 df =  pd.read_csv('/mnt/c/Users/Patrick/Documents/Projects/sp500-stockPrice-pipeline/database/Data/company_info.csv' , usecols=['Ticker'])
 ticker_list = list(df['Ticker'])
-url_list = list('https://finance.yahoo.com/quote/%5EGSPC?p=%5EGSPC')
+url_list = ['https://finance.yahoo.com/quote/%5EGSPC?p=%5EGSPC']
 
 for i in ticker_list:
-      url_list.append(f'https://finance.yahoo.com/quote/{i}?p={i}')
+    if i != "^GSPC":
+        url_list.append(f'https://finance.yahoo.com/quote/{i}?p={i}')
 
 class stockPrices(scrapy.Spider):
     name = 'stock_prices'
