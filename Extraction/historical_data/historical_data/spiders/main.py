6e94c9c60977dddaf66cc320e5eb88adae833e6b
Patrick Bacza <Patrick.Bacza@outlook.com>
2023-06-30 21:38:37 -0400

    Started grabbing historical data for sectors

--- Extraction/historical_data/historical_data/spiders/main.py
+++ Extraction/historical_data/historical_data/spiders/main.py
@@ -3,22 +3,23 @@ import pandas as pd
 import csv
 from datetime import date
 
-# df = pd.read_csv('/Data/company_info.csv' , usecols=['Ticker'])
-# ticker_list = list(df['Ticker'])
-# url_list = []
+df = pd.read_csv('C:\\Users\\Patrick\\Documents\\Projects\\sp500-stockPrice-pipeline\\database\\Data\\Indices_dimension.csv' , usecols=['Ticker'])
 
-# for i in ticker_list:
-#     url_list.append(f'https://finance.yahoo.com/quote/{i}/history?period1=1671753600&period2=1671840000&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true')
+df['Ticker'] = df['Ticker'].str.replace('^' , '')
 
+ticker_list = list(df['Ticker'])
+url_list = []
+
+for i in ticker_list:
+    url_list.append(f'https://finance.yahoo.com/quote/%5E{i}/history?period1=1652918400&period2=1688083200&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true')
 
 class stockPrices(scrapy.Spider):
     name = 'historical_data'
 
-    start_urls = ['https://finance.yahoo.com/quote/%5EGSPC/history?period1=1653004800&period2=1661990400&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true'] #url_list
-
+    start_urls = url_list
     def parse(self, response):
          
-        with open('Output_3.csv', 'a', newline='') as csvfile:
+        with open('sectors_data_output_1.csv', 'a', newline='') as csvfile:
             writer = csv.writer(csvfile)
 
             rows = response.css('table[data-test="historical-prices"] tr')
