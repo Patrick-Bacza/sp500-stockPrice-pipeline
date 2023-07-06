e29a2b42c4f2614484ef2e2361831de97a4c39cd
Patrick Bacza <Patrick.Bacza@outlook.com>
2023-07-05 21:02:51 -0400

    Fixed data errors

--- Extraction/historical_data/historical_data/spiders/main.py
+++ Extraction/historical_data/historical_data/spiders/main.py
@@ -3,23 +3,24 @@ import pandas as pd
 import csv
 from datetime import date
 
-df = pd.read_csv('C:\\Users\\Patrick\\Documents\\Projects\\sp500-stockPrice-pipeline\\database\\Data\\Indices_dimension.csv' , usecols=['Ticker'])
+# df = pd.read_csv('C:\\Users\\Patrick\\Documents\\Projects\\sp500-stockPrice-pipeline\\database\\Data\\company_info.csv' , usecols=['Ticker'])
 
-df['Ticker'] = df['Ticker'].str.replace('^' , '')
+# df['Ticker'] = df['Ticker'].str.replace('^' , '')
 
-ticker_list = list(df['Ticker'])
+# ticker_list = list(df['Ticker'])
+# url_list = []
+
+# for i in ticker_list:
+#     url_list.append(f'https://finance.yahoo.com/quote/{i}/history?period1=1659398400&period2=1659571200&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true')
+
 url_list = []
-
-for i in ticker_list:
-    url_list.append(f'https://finance.yahoo.com/quote/%5E{i}/history?period1=1652918400&period2=1663027200&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true')
-
 class stockPrices(scrapy.Spider):
     name = 'historical_data'
 
     start_urls = url_list
     def parse(self, response):
          
-        with open('sectors_data_output_3.csv', 'a', newline='') as csvfile:
+        with open('BRKB_BFB_AOS.csv', 'a', newline='') as csvfile:
             writer = csv.writer(csvfile)
 
             rows = response.css('table[data-test="historical-prices"] tr')
