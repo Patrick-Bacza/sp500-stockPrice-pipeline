f65c4626989ea7b07bd2ce47b3bad29ce2f2865a
Patrick Bacza <Patrick.Bacza@outlook.com>
2023-07-15 22:23:24 -0400

    Added in airflow DAG to repo

--- database/postgres_scripts/fact_table_creation script.sql
+++ database/postgres_scripts/fact_table_creation script.sql
@@ -1,18 +1,12 @@
 CREATE TABLE daily_stock_prices (
 	id SERIAL PRIMARY KEY,
 	date DATE,
-	ticker VARCHAR(5),
+	ticker VARCHAR(11),
 	open_price FLOAT,
 	close_price FLOAT,
-	volume VARCHAR,
+	volume BIGINT,
 	previous_close FLOAT,
 	intraday_low FLOAT,
 	intraday_high FLOAT,
-	bid_price FLOAT,
-	ask_price FLOAT
-
-
-
 )
 
-drop table test
