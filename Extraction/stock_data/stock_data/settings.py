4a015f8bd6ae3263fe7cc648344df43e93aab3c3
Patrick Bacza <Patrick.Bacza@outlook.com>
2023-07-17 22:20:35 -0400

    Increased get delay to 2 seconds

--- Extraction/stock_data/stock_data/settings.py
+++ Extraction/stock_data/stock_data/settings.py
@@ -35,7 +35,7 @@ AWS_SECRET_ACCESS_KEY = 'n5i+3vH7vmLJ/nDPHQDRK1O9HYwDgL1+j0+1X3fZ'
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 1
+DOWNLOAD_DELAY = 2
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
